fc3681b636942c6ab7a08b5ed8765e8cd811dd29
Osiris <noirozr@icloud.com>
2016-05-29 22:51:08 +0800

    first commit

---
+++ iCloudDemo/MyCloudDocument.swift
@@ -0,0 +1,13 @@
+//
+//  MyCloudDocument.swift
+//  iCloudDemo
+//
+//  Created by Noirozr on 16/5/29.
+//  Copyright © 2016年 Yongjia Liu. All rights reserved.
+//
+
+import UIKit
+
+class MyCloudDocument: UIDocument {
+    
+}
---
+++ iCloudDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCloudDemo
+//
+//  Created by Noirozr on 16/5/29.
+//  Copyright © 2016年 Yongjia Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
